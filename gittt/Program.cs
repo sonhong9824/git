f4657bed9eeb2168f01d111d35ff27a93affdb36
sonpro9824 <sonpro9824@gmail.com>
2024-01-26 23:33:41 +0700

    new

---
+++ gittt/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gittt
+{
+    internal class Program
+    {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+    }
+}
